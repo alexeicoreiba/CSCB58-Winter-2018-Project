7f3e2118e249901ed746ce2d07115bcdc106f04b
Kanstantsin Ilioukevitch <kostya.iliouk@hotmail.com>
2018-04-04 16:49:19 -0400

    Making it more accurate

--- sensor.v
+++ sensor.v
@@ -82,25 +82,15 @@ module usensor(distance, trig, echo, clock);
 			    begin
 					echo_high <= 1;
 					echo_timer <= echo_timer + 1;
-					//////////////////////////////////////////////////////
-					// CLOCK_50 -> 50 000 000 clock cycles per second
-					// let n = number of cycles
-					// speed of sound in air: 340m/s
-					// n / 50 000 000 = num of seconds
-					// num of seconds * 340m/s = meters
-					// meters * 100 = cm ~ distance to object and back
-					// So we divide by 2 to get distance to object
-					// 1/ 50 000 000 * 340 * 100 / 2 = 0.00034
-					// n * 0.00034 = n * 34/100 000 = n / (100 000/34)
-					// = 2941
-					// To make up for sensor inaccuracy and simple math
-					// we round down to 2900
+					//echo_shift10 <= echo_timer >> 10;
+					//echo_shift12 <= echo_timer >> 12;
 					temp_distance <= (echo_timer / 2900);
-					//////////////////////////////////////////////////////
+					//distance <= (echo_shift10 - echo_shift12) >> 1;
+					//distance <= (echo_timer >> 13) * 3;
 			    end
         else
           begin
-				distance <= temp_distance;
+				distance <= temp_distance + 2'd2;
 				echo_timer <= 0;
 				trig_timer <= 0;
 				echo_sense <= 0;
@@ -487,4 +477,3 @@ case(address)
 endcase
 endmodule
 
-/////////////////////////////////////////////////////
